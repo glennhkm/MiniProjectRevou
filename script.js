ebe96349877ce650e9b527a1593296f53f36acd6
glennhkm <glenn@mhs.usk.ac.id>
2023-07-19 23:29:25 +0700

    Membersihkan code

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function hasilperhitungan(){
     
     //membuat condition agar hasilnya muncul
     if(!isNaN(hasil)){
-        document.getElementById("tulisanhasil").innerHTML = "Hasil"
         document.getElementById("hasilangka").innerHTML = hasil.toFixed(2);
         if(hasil < 18.5){
             document.getElementById("hasilpenjelasan").innerHTML = "Anda kekurangan berat badan";
@@ -21,18 +20,5 @@ function hasilperhitungan(){
         else{
             document.getElementById("hasilpenjelasan").innerHTML = "Anda obesitas";
         }
-        document.getElementById("penjelasanakhir").innerHTML = "BMI tidak sepenuhnya mewakili diagnosis menyeluruh dari kesehatan tubuh dan resiko penyakit seseorang. Anda perlu konsultasi lebih lanjut mengenai resiko dan kekhawatiran Anda terkait dengan berat badan Anda"
     }
 }
-
-// function hasilperhitungan() {
-//     var beratbadan = parseFloat(document.getElementById("BB").value);
-//     var tinggibadan = parseFloat(document.getElementById("TB").value) / 100; // Mengubah tinggi dari cm menjadi meter
-
-//     if (beratbadan > 0 && tinggibadan > 0) {
-//         var hasil = beratbadan / (tinggibadan ** 2);
-//         document.getElementById("hasil").innerHTML = hasil.toFixed(2);
-//     } else {
-//         document.getElementById("hasil").innerHTML = "Silakan masukkan berat dan tinggi yang valid.";
-//     }
-// }
